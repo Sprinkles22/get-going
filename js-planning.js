674f9fc95c25ea3a1fccb861ba00bdb4efd2ef63
ryan welch <ryan welch>
2016-08-04 17:01:28 -0600

    doin module pattern, not workign beware line 37

--- js-planning.js
+++ js-planning.js
@@ -3,130 +3,128 @@
 // 3. Make api call, make an array or object to pass to the map, with each individual zip code's cord array
 // 4. draw each zip code
 // init() -> getZips() -> getCordsFromApi() -> drawOntoMap()
+(function() {
+    var boundries = {
+        /*config defaults*/
+        mapConfig: {
+            mapSelector: "#map",
+            zoom: 12,
+            center: {
+                lat: 39.7456423,
+                lng: -104.9923426
+            },
+            mapTypeId: google.maps.MapTypeId.TERRAIN,
+            strokeColor: 'gold',
+            strokeOpacity: 1,
+            strokeWeight: '5px',
+            fillColor: 'black',
+            fillOpacity: 1
+        }
+        //actually make the ajax call, takes individual zip codes
+        callApi: function(zipCode) {
+                $.ajax({
+                        type: 'GET',
+                        url: 'http://boundaries.io/geographies/postal-codes?search=' + zipCode,
+                        headers: {
+                            'X-Requested-With': 'XMLHttpRequest',
+                            'Accept-Encoding': 'gzip, deflate, sdch',
+                            'Accept': 'application/json, text/javascript, */*; q=0.01'
+                        }
 
-var scope = {}
-    /*config defaults*/
-scope.mapConfig = {
-    mapSelector: "#map",
-    zoom: 12,
-    center: {
-        lat: 39.7456423,
-        lng: -104.9923426
-    },
-    mapTypeId: google.maps.MapTypeId.TERRAIN,
-    strokeColor: 'gold',
-    strokeOpacity: 1,
-    strokeWeight: '5px',
-    fillColor: 'black',
-    fillOpacity: 1
-}
-
-function getCordsFromApi(arrayOfZipCodesFromUser) {
-    //this is the object we're gonna return
-    var coOrdsFromApi = {}
-
-    //actually make the ajax call, takes individual zip codes
-    function callApi(zipCode) {
-        $.ajax({
-                type: 'GET',
-                url: 'http://boundaries.io/geographies/postal-codes?search=' + zipCode,
-                headers: {
-                    'X-Requested-With': 'XMLHttpRequest',
-                    'Accept-Encoding': 'gzip, deflate, sdch',
-                    'Accept': 'application/json, text/javascript, */*; q=0.01'
-                }
-
-            })
-            //if it works
-            .success(response) {
-                coOrdsFromApi.zipCode = proccessApiResponse(response);
+                    })
+                    //if it works
+                    .success(response) {
+                        coOrdsFromApi.zipCode = proccessApiResponse(response);
+                    }
             }
-    }
+            //take the dumb ass response and pull the coords for drawing out of it.
+        proccessApiResponse: function(response) {
+            //return this when done
+            var formatedCords = [];
+            //reponse object where the coords are kept
+            var responseCoords = d[0]['geometry']['coordinates'];
 
-    //take the dumb ass response and pull the coords for drawing out of it.
-    function proccessApiResponse(response) {
-        //return this when done
-        var formatedCords = [];
-        //reponse object where the coords are kept
-        var responseCoords = d[0]['geometry']['coordinates'];
-
-        //uhhhh this is copied from the protoype, needs to actually work.
-        for (var i = 0; i < responseCoords.length; i++) {
-            for (var k = 0; k < responseCoords[i].length; k++) {
-                if (responseCoords.length > 1) {
-                    for (var m = 0; m < responseCoords[i].length; m++) {
+            //uhhhh this is copied from the protoype, needs to actually work.
+            for (var i = 0; i < responseCoords.length; i++) {
+                for (var k = 0; k < responseCoords[i].length; k++) {
+                    if (responseCoords.length > 1) {
+                        for (var m = 0; m < responseCoords[i].length; m++) {
+                            formatedCords.push({
+                                lat: responseCoords[i][k][m][1],
+                                lng: responseCoords[i][k][m][0]
+                            });
+                        }
+                    } else {
                         formatedCords.push({
-                            lat: responseCoords[i][k][m][1],
-                            lng: responseCoords[i][k][m][0]
+                            lat: responseCoords[i][k][1],
+                            lng: responseCoords[i][k][0]
                         });
                     }
-                } else {
-                    formatedCords.push({
-                        lat: responseCoords[i][k][1],
-                        lng: responseCoords[i][k][0]
-                    });
-                }
 
+                }
+            }
+
+            //should just be a big old array full of {lat: 11111, lng: 22222}
+            return formatedCords
+        }
+        getCordsFromApi: function(arrayOfZipCodesFromUser) {
+            //this is the object we're gonna return
+            var coOrdsFromApi = {}
+                //lets take our array of zip codes and call the api for each one.
+            for (var i = 0; i < arrayOfZipCodesFromUser.length; i++) {
+                callApi(arrayOfZipCodesFromUser[i]);
+            }
+
+            return coOrdsFromApi //something like this {'11111': [ {lat:222222, lng:33333}, {lat:222223, lng:33334} ]   } 
+        }
+
+        //get value from a form field and turn it into a list we can use
+            getZips: function(el) {
+            var userInputZipCodes = /*get value from a form field -> el*/
+                var arrayOfZipCodesFromUser = []; //array of ints or strings we return
+
+            //comma, semi colon or newline delinated list
+            function getZipCodes(userInputZipCodes) {
+                arrayOfZipCodesFromUser.push( /*something goes in here*/ )
+            }
+
+            return arrayOfZipCodesFromUser
+        }
+
+        //actually draw it now
+            drawOntoMap: function(coOrdsFromApi) {
+
+            for (zips in coOrdsFromApi) {
+                //draw
+
+                /*placeholder*/
+                var newPoints = new google.maps.Polygon({
+                    paths: zips, // This comes from the coOrdsFromApi object
+                    strokeColor: boundries.mapConfig.strokeColor,
+                    strokeOpacity: boundries.mapConfig.strokeOpacity,
+                    strokeWeight: boundries.mapConfig.strokeWeight,
+                    fillColor: boundries.mapConfig.fillColor,
+                    fillOpacity: boundries.mapConfigfillOpacity
+                });
             }
         }
 
-        //should just be a big old array full of {lat: 11111, lng: 22222}
-        return formatedCords
+            init: function(config) {
+            //set everything up???
+            //show google map?   
+            //config  
+
+            /*placeholder*/
+            //use jquery or force map to have an id or class?
+            map = new google.maps.Map($(boundries.mapConfig.mapSelector)[0], {
+                zoom: boundries.mapConfig.zoom,
+                center: boundries.mapConfig.center,
+                mapTypeId: boundries.mapConfig.mapTypeId
+
+            });
+        }
+
     }
-
-    //lets take our array of zip codes and call the api for each one.
-    for (var i = 0; i < arrayOfZipCodesFromUser.length; i++) {
-        callApi(arrayOfZipCodesFromUser[i]);
-    }
-
-    return coOrdsFromApi //something like this {'11111': [ {lat:222222, lng:33333}, {lat:222223, lng:33334} ]   } 
-}
-
-//get value from a form field and turn it into a list we can use
-function getZips() {
-    var userInputZipCodes = /*get value from a form field*/
-        var arrayOfZipCodesFromUser = []; //array of ints or strings we return
-
-    //comma, semi colon or newline delinated list
-    function getZipCodes(userInputZipCodes) {
-        arrayOfZipCodesFromUser.push( /*something goes in here*/ )
-    }
-
-    return arrayOfZipCodesFromUser
-}
-
-//actually draw it now
-function drawOntoMap(coOrdsFromApi) {
-
-    for (zips in coOrdsFromApi) {
-        //draw
-
-        /*placeholder*/
-       var newPoints = new google.maps.Polygon({
-            paths: zips, // This comes from the coOrdsFromApi object
-            strokeColor: scope.mapConfig.strokeColor,
-            strokeOpacity: scope.mapConfig.strokeOpacity,
-            strokeWeight: scope.mapConfig.strokeWeight,
-            fillColor: scope.mapConfig.fillColor,
-            fillOpacity: scope.mapConfigfillOpacity
-        });
-    }
-}
-
-function init(config) {
-    //set everything up???
-    //show google map?   
-    //config  
-
-    /*placeholder*/
-    //use jquery or force map to have an id or class?
-    map = new google.maps.Map($(scope.mapConfig.mapSelector)[0], {
-        zoom: scope.mapConfig.zoom,
-        center: scope.mapConfig.center,
-        mapTypeId: scope.mapConfig.mapTypeId
-
-    });
-}
-
-
-init();
+    init();
+    return boundries
+})();
